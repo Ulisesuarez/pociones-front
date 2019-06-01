bf0f4ec6fb114e84692a62ac17e5d9c5e8a8236f
Valenti <valendieguez89@gmail.com>
2019-06-01 12:45:57 +0200

    warnings fix

--- pociones-front/src/views/PotionView.vue
+++ pociones-front/src/views/PotionView.vue
@@ -79,8 +79,8 @@ export default Vue.extend({
         drink: {},
         comments: [],
         user: {},
-        opinion: ''
-        }
+        opinion: '',
+        };
     },
     computed: {
         recipeId() {
